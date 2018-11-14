5024bb7fe70e9621a1590fc615b61eaedc0a942f
LLewis <lorimlewis@gmail.com>
2018-11-14 23:00:17 +0000

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+package studentDatabaseApp;
+
+public class Student {
+
+		//constructor: prompt user to enter student's name and year
+	
+		//generate ID
+	
+	//enroll in courses
+	
+	//view balance and pay tuition
+	
+	//show status
+}
---
+++ StudentDatabaseApp.java
@@ -0,0 +1,11 @@
+package studentDatabaseApp;
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+		//ask how many students we want to add
+		
+		//create n number of new students
+	}
+
+}
