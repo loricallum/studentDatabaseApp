f212e4f18491b81aaee811d36056b3e601eea3e6
LLewis <lorimlewis@gmail.com>
2018-12-26 15:53:21 +0000

    Update StudentDatabaseApp.java

--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -1,11 +1,14 @@
-package studentDatabaseApp;
+package studentdatabaseapp;
 
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		//ask how many students we want to add
+		Student stu1 = new Student();
+		stu1.enroll();
+		stu1.payTuition();
+		System.out.println(stu1.toString());
 		
-		//create n number of new students
 	}
-
+		
+			
 }
